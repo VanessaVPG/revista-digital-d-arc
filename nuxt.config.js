6bb5db771dd5efc3cfbd630c1ff1eff351978ab6
vanessavpg <vanessa.pg026@gmail.com>
2022-11-27 02:22:01 -0300

    particles

--- nuxt.config.js
+++ nuxt.config.js
@@ -154,8 +154,6 @@ export default {
     '~plugins/helpers.js',
     '~plugins/vue-the-mask.js',
     { src: '~/plugins/vue-screen.client.js', mode: 'client' },
-
-    { src: '@/plugins/particles', ssr: false },
   ],
 
   render: {
